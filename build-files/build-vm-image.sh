3234c925a9ed05d029a7bea844a3c204c4c70003
Ajay Tiwari <ajay@saviantconsulting.com>
2017-05-22 18:14:13 +0530

    Changed the Credentials

--- build-files/build-vm-image.sh
+++ build-files/build-vm-image.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+##echo "************* set environment vars"
+##export ARM_CLIENT_ID=$1
+##export ARM_CLIENT_SECRET=$2
+##export ARM_RESOURCE_GROUP=$3
+##export ARM_STORAGE_ACCOUNT=$4
+##export ARM_SUBSCRIPTION_ID=$5
+##export ARM_TENANT_ID=$6
+##export ADMIN_USERNAME=$7
+##export ADMIN_PASSWORD=$8
+
 echo "************* set environment vars"
 export ARM_CLIENT_ID=$1
-export ARM_CLIENT_SECRET=$2
-export ARM_RESOURCE_GROUP=$3
-export ARM_STORAGE_ACCOUNT=$4
-export ARM_SUBSCRIPTION_ID=$5
-export ARM_TENANT_ID=$6
-export ADMIN_USERNAME=$7
-export ADMIN_PASSWORD=$8
+export ARM_CLIENT_SECRET=rBg9eFqD0rHugveAzGfKr06MOTzVIvDgk/Lb++FuMnY=
+export ARM_RESOURCE_GROUP=mpointpoc
+export ARM_STORAGE_ACCOUNT=mpoint
+export ARM_SUBSCRIPTION_ID=28d69262-04f8-4974-8f5f-953cc6f18549
+export ARM_TENANT_ID=72e1d49a-c9eb-4554-96b9-e764bddd3340
+export ADMIN_USERNAME=adminuser
+export ADMIN_PASSWORD=Saviant@1234
+
 
 rm packer-build-output.log
 
