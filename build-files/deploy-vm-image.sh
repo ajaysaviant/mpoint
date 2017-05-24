32348206caeec01a610369e35212afeaf13e4e2d
Ajay Tiwari <ajay@saviantconsulting.com>
2017-05-25 00:18:31 +0530

    Added azure-cli command

--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cat azuredeploy.parameters.json
-az group deployment create -f azuredeploy.json -e azuredeploy.parameters.json  -g $1 -n rocdashdeploy$2
+azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json -g mpointpoc -n rocdashdeploy$2
