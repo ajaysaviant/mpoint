0e5e0663d1b8de5f5cd049b34daa83bf699bc50a
Ajay Tiwari <ajay@saviantconsulting.com>
2017-05-24 15:53:56 +0530

    Changed command from azure to az

--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cat azuredeploy.parameters.json
-azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json  -g $1 -n rocdashdeploy$2
+az group deployment create -f azuredeploy.json -e azuredeploy.parameters.json  -g $1 -n rocdashdeploy$2
