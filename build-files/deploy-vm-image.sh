43e552d89ef0837c5ae9f35c120bf3c12926dd19
Ajay Tiwari <ajay@saviantconsulting.com>
2017-05-25 19:50:05 +0530

    Added Azure login command

--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cat azuredeploy.parameters.json
+azure account set 28d69262-04f8-4974-8f5f-953cc6f18549 
 azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json -g mpointpoc -n rocdashdeploy$2
