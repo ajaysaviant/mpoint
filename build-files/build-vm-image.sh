ac5dfc4facd6dfc568715c6aa5b26586f113be7b
Ajay Tiwari <ajay@saviantconsulting.com>
2017-05-30 12:44:35 +0530

    Changed the Azure Portal to mpoint

--- build-files/build-vm-image.sh
+++ build-files/build-vm-image.sh
@@ -11,14 +11,14 @@
 ##export ADMIN_PASSWORD=$8
 
 echo "************* set environment vars"
-export ARM_CLIENT_ID=04287572-5a24-4e77-89b6-27de62925515
-export ARM_CLIENT_SECRET=XDYF5P6v4wTwL7YCcjS2XFtrkFSfJoJ5Th2kqUUU4Uw=
+export ARM_CLIENT_ID=a6e0fa5f-707a-4615-89e0-01a25d8f227b
+export ARM_CLIENT_SECRET=CLUvVsojOWL1udp7POx2bsUzFiNLvAp7Zc0mBOdvCUc=
 export ARM_RESOURCE_GROUP=mpointpoc
 export ARM_STORAGE_ACCOUNT=mpoint
-export ARM_SUBSCRIPTION_ID=28d69262-04f8-4974-8f5f-953cc6f18549
-export ARM_TENANT_ID=72e1d49a-c9eb-4554-96b9-e764bddd3340
-export ADMIN_USERNAME=adminuser
-export ADMIN_PASSWORD=Saviant@1234
+export ARM_SUBSCRIPTION_ID=2951e8ec-d8fe-4ccf-9b2d-60c850790d4b
+export ARM_TENANT_ID=9b1573f5-e2ee-4d25-97dd-51b6f4c45b99
+export ADMIN_USERNAME=mpointadmin
+export ADMIN_PASSWORD=Gogreen@456
 
 
 rm packer-build-output.log
--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cat azuredeploy.parameters.json
-azure account set 28d69262-04f8-4974-8f5f-953cc6f18549 
 azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json -g mpointpoc -n rocdashdeploy$2
