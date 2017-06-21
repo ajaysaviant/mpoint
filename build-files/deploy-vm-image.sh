9c9a2c46184c43e3ef535cc11f4df9060038af3e
Ajay Tiwari <ajay@saviantconsulting.com>
2017-06-21 15:54:44 +0530

    recreated VM scale Set

--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cat azuredeploy.parameters.json
-Remove-AzureRmVmss -ResourceGroupName mPoint_Scaleset_29052017  -VMScaleSetName mpointsca -Force
 azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json -g mPoint_Scaleset_29052017 -n rocdashdeploy$2
