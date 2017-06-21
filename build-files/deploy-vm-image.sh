deccf1d61042c3199067212448a00d4e14a45aee
Ajay Tiwari <ajay@saviantconsulting.com>
2017-06-21 14:28:01 +0530

    Added delete scaleset command

--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cat azuredeploy.parameters.json
+Remove-AzureRmVmss -ResourceGroupName mPoint_Scaleset_29052017  -VMScaleSetName mpointsca -Force
 azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json -g mPoint_Scaleset_29052017 -n rocdashdeploy$2
