7d09e5935dfb0e9227b08ba3ef373ac25a304d72
Ajay Tiwari <ajay@saviantconsulting.com>
2017-06-22 15:02:16 +0530

    changed resource group

--- build-files/deploy-vm-image.sh
+++ build-files/deploy-vm-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cat azuredeploy.parameters.json
-azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json -g mPoint_Scaleset_29052017 -n rocdashdeploy$2
+azure group deployment create -f azuredeploy.json -e azuredeploy.parameters.json -g mpoint_scaleset_production -n mpointdeploy$2
