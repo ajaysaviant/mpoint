d9facad274951206515523e540c78d455ede9bfa
Ajay Tiwari <ajay@saviantconsulting.com>
2017-06-27 14:58:45 +0530

    Changed the VM configuration

--- build-files/build-vm-image.sh
+++ build-files/build-vm-image.sh
@@ -10,15 +10,15 @@ export ARM_TENANT_ID=$6
 export ADMIN_USERNAME=$7
 export ADMIN_PASSWORD=$8
 
-echo "************* set environment vars"
-export ARM_CLIENT_ID=a6e0fa5f-707a-4615-89e0-01a25d8f227b
-export ARM_CLIENT_SECRET=CLUvVsojOWL1udp7POx2bsUzFiNLvAp7Zc0mBOdvCUc=
-export ARM_RESOURCE_GROUP=mpoint_scaleset_production
-export ARM_STORAGE_ACCOUNT=mpointvhdstorage
-export ARM_SUBSCRIPTION_ID=2951e8ec-d8fe-4ccf-9b2d-60c850790d4b
-export ARM_TENANT_ID=9b1573f5-e2ee-4d25-97dd-51b6f4c45b99
-export ADMIN_USERNAME=mpointadmin
-export ADMIN_PASSWORD=Gogreen@456
+##echo "************* set environment vars"
+##export ARM_CLIENT_ID=a6e0fa5f-707a-4615-89e0-01a25d8f227b
+##export ARM_CLIENT_SECRET=CLUvVsojOWL1udp7POx2bsUzFiNLvAp7Zc0mBOdvCUc=
+##export ARM_RESOURCE_GROUP=mpoint_scaleset_production
+##export ARM_STORAGE_ACCOUNT=mpointvhdstorage
+##export ARM_SUBSCRIPTION_ID=2951e8ec-d8fe-4ccf-9b2d-60c850790d4b
+##export ARM_TENANT_ID=9b1573f5-e2ee-4d25-97dd-51b6f4c45b99
+##export ADMIN_USERNAME=mpointadmin
+##export ADMIN_PASSWORD=Gogreen@456
 
 
 rm packer-build-output.log
